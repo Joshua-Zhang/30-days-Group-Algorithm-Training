80f20189bca58db46bc29569557ff010de73d004
Egbert <egbert1121@gmail.com>
2017-01-16 23:36:29 +1100

    be carful the syntax

--- Yixuan/16_01_17/strstr.java
+++ Yixuan/16_01_17/strstr.java
@@ -1,64 +1,30 @@
-// Second Version
-class Solution {
-   /**
-    * @param x: An integer
-    * @return: The sqrt of x
-    * @link: http://www.lintcode.com/en/problem/sqrtx/
-    * @author: Egbert Li
-    * @Tag: Facebook
-    */
-   public int sqrt(int x) {
-      if (x <= 0) {
-         return 0;
-      }
-      int start = 1;
-      int end = x;
-      while (start + 1 < end) {
-         int mid = start + (end - start) / 2;
-         if (mid * mid == x) {
-            return mid;
-         } else {
-            // to avoid overflow
-            if (x / mid < mid) {
-               end = mid;
-            } else {
-               start = mid;
-            }
-         }
-      }
-      if (end * end <= x && end >= x) {
-         return end;
-      }
-      return start;
-   }
-}
-
-// First Version
 class Solution {
     /**
-     * @param x: An integer
-     * @return: The sqrt of x
-     * @link: http://www.lintcode.com/en/problem/sqrtx/
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     * @link: http://www.lintcode.com/en/problem/strstr/
      * @author: Egbert Li
-     * @Tag: Facebook
+     * @Tag: Facebook, Phone interview questions
      */
-    public int sqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
-        long start = 1;
-        long end = x;
-        while (start + 1 < end) {
-            long mid = start + (end - start) / 2;
-            if (mid * mid  > x) {
-                end = mid;
-            } else {
-                start = mid;
+   public int strStr(String source, String target) {
+      if (source == null || target == null) {
+         return -1;
+      }
+      int source_length = source.length();
+      int target_length = target.length();
+      for (int i = 0; i < source_length - target_length + 1; i++) {
+         int j = 0;
+         for (j = 0; j < target_length; j++) {
+            if (source.charAt(i + j) !=  target.charAt(j)) {
+               break;
             }
-        }
-        if (end * end <= x && end >= start) {
-            return (int) end;
-        }
-        return (int) start;
-    }
+         }
+         if (j == target_length) {
+            return i;
+         }
+      }
+      return -1;
+   }
 }
