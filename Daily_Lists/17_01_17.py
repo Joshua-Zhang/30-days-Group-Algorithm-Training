9a8cab3ddd384cde0f3fc04a0ad85670500caf06
Egbert <egbert1121@gmail.com>
2017-01-17 20:48:02 +1100

    17 Jan 17 questions

---
+++ Daily_Lists/17_01_17.py
@@ -0,0 +1,22 @@
+Continue Binary Search Questions
+
+1. Find Minimum in Rotated Sorted Array
+http://www.lintcode.com/en/problem/find-minimum-in-rotated-sorted-array/
+
+2.Find Peak Element
+http://www.lintcode.com/en/problem/find-peak-element/
+
+3. First Bad Version
+http://www.lintcode.com/en/problem/first-bad-version/
+
+4. Search in Rotated Sorted Array
+http://www.lintcode.com/en/problem/search-in-rotated-sorted-array/
+
+5. Total Occurrence of Target
+http://www.lintcode.com/en/problem/total-occurrence-of-target/
+
+6. Closest Number in sorted Array
+http://www.lintcode.com/en/problem/closest-number-in-sorted-array/
+
+7. Copy Books
+http://www.lintcode.com/en/problem/copy-books/
