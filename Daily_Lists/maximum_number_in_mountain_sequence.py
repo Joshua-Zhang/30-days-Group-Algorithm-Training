f5b1eb7b7cb335678d2b5985bd986b922360ead1
Egbert <egbert1121@gmail.com>
2017-01-19 00:55:46 +1100

    question and header in C++ and python

---
+++ Daily_Lists/maximum_number_in_mountain_sequence.py
@@ -0,0 +1,30 @@
+Given a mountain sequence of n integers which increase
+ firstly and then decrease, find the mountain top.
+
+Have you met this question in a real interview? Yes
+Example
+Given nums = [1, 2, 4, 8, 6, 3] return 8
+Given nums = [10, 9, 8, 7], return 10
+
+# Python
+class Solution:
+    # @param {int[]} nums a mountain sequence which increase firstly and then decrease
+    # @return {int} then mountain top
+    # @link: http://www.lintcode.com/en/problem/maximum-number-in-mountain-sequence/
+    # @author: Egbert Li
+    def mountainSequence(self, nums):
+
+
+# C++
+class Solution {
+public:
+    /**
+     * @param nums a mountain sequence which increase firstly and then decrease
+     * @return then mountain top
+     * @author: Egbert Li
+     * @link: http://www.lintcode.com/en/problem/maximum-number-in-mountain-sequence/
+     */
+    int mountainSequence(vector<int>& nums) {
+
+    }
+};
