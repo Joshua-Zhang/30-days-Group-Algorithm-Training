1bbb31f78476764d5760e12dc980c4d4f330633c
Egbert <egbert1121@gmail.com>
2017-01-18 18:06:07 +1100

    question and header in C++, java, python

---
+++ Daily_Lists/total_occurrence_of_target/question.py
@@ -0,0 +1,53 @@
+Given a target number and an integer array sorted in ascending
+order. Find the total number of occurrences of target in the array.
+
+Have you met this question in a real interview? Yes
+Example
+Given [1, 3, 3, 4, 5] and target = 3, return 2.
+
+Given [2, 2, 3, 4, 6] and target = 4, return 1.
+
+Given [1, 2, 3, 4, 5] and target = 6, return 0.
+
+Challenge
+Time complexity in O(logn)
+
+
+
+# C++
+class Solution {
+public:
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     * @link: http://www.lintcode.com/en/problem/total-occurrence-of-target/
+     * @author: Egbert Li
+     */
+    int totalOccurrence(vector<int>& A, int target) {
+
+    }
+};
+
+# Python
+class Solution:
+    # @param {int[]} A an integer array sorted in ascending order
+    # @param {int} target an integer
+    # @return {int} an integer
+    # @link: http://www.lintcode.com/en/problem/total-occurrence-of-target/
+    # @author: Egbert Li
+    def totalOccurrence(self, A, target):
+
+# Java
+public class Solution {
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     * @link: http://www.lintcode.com/en/problem/total-occurrence-of-target/
+     * @author: Egbert Li
+     */
+    public int totalOccurrence(int[] A, int target) {
+
+    }
+}
