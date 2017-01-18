c71ce884c69b425a723ca2ec0aee29d7dfd67631
Egbert <egbert1121@gmail.com>
2017-01-18 17:01:14 +1100

    18 Jan 2017 questions, Good luck

---
+++ Daily_Lists/18_01_17.py
@@ -0,0 +1,28 @@
+# Continue BS
+
+1. Drop Eggs
+http://www.lintcode.com/en/problem/drop-eggs/
+
+2. Maximum Average Subarray
+http://www.lintcode.com/en/problem/maximum-average-subarray/
+
+3. Maximum Number in Mountain sequence
+http://www.lintcode.com/en/problem/maximum-number-in-mountain-sequence/
+
+4. K Closest Numbers In Sorted Array
+http://www.lintcode.com/en/problem/k-closest-numbers-in-sorted-array/
+
+5. Divide Two Integers
+http://www.lintcode.com/en/problem/divide-two-integers/
+
+6. Search a 2D Matrix II
+http://www.lintcode.com/en/problem/search-a-2d-matrix-ii/
+
+7. Search for a Range
+http://www.lintcode.com/en/problem/search-for-a-range/
+
+8. Find Minimum in Rotated Sorted Array II
+http://www.lintcode.com/en/problem/find-minimum-in-rotated-sorted-array-ii/
+
+9. Search in Rotated Sorted Array II
+http://www.lintcode.com/en/problem/search-in-rotated-sorted-array-ii/
