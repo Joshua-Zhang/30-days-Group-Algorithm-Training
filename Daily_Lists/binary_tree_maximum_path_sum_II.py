bb6ea752c226143fa06738a315e6e75960506953
Egbert <egbert1121@gmail.com>
2017-01-20 00:18:26 +1100

    quenstions

---
+++ Daily_Lists/binary_tree_maximum_path_sum_II.py
@@ -0,0 +1,56 @@
+Binary Tree Maximum Path Sum II
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given a binary tree, find the maximum path sum from root.
+
+The path may end at any node in the tree and contain at least one node in it.
+
+Have you met this question in a real interview? Yes
+Example
+Given the below binary tree:
+
+  1
+ / \
+2   3
+return 4. (1->3)
+
+# Java
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of binary tree.
+     * @return an integer
+     */
+    public int maxPathSum2(TreeNode root) {
+        // Write your code here
+    }
+}
+
+# Python
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+class Solution:
+    """
+    @param root the root of binary tree.
+    @return an integer
+    """
+    def maxPathSum2(self, root):
+        # Write your code here
---
+++ Daily_Lists/inorder_successor_in_binary_search_tree.py
@@ -0,0 +1,40 @@
+Given a binary search tree (See Definition) and a node in it, find the in-order successor of that node in the BST.
+
+If the given node has no in-order successor in the tree, return null.
+
+ Notice
+
+It's guaranteed p is one node in the given tree. (You can directly compare the memory address to find p)
+
+Have you met this question in a real interview? Yes
+Example
+Given tree = [2,1] and node = 1:
+
+  2
+ /
+1
+return node 2.
+
+Given tree = [2,1,3] and node = 2:
+
+  2
+ / \
+1   3
+return node 3.
+
+# python
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    """
+    @param root <TreeNode>: The root of the BST.
+    @param p <TreeNode>: You need find the successor node of p.
+    @return <TreeNode>: Successor of p.
+    """
+    def inorderSuccessor(self, root, p):
+        # write your code here
