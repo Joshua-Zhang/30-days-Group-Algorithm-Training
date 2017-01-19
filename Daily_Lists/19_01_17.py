e5b5207328055eee0cbb4e788cb3f2acf849daa7
Egbert <egbert1121@gmail.com>
2017-01-19 21:04:41 +1100

    19 Jan 2017 questions

---
+++ Daily_Lists/19_01_17.py
@@ -0,0 +1,19 @@
+Binary Search Tree and D & C
+
+1. Maximum Depth of Binary Tree
+http://www.lintcode.com/en/problem/maximum-depth-of-binary-tree/
+
+2. Balanced Binary Tree
+http://www.lintcode.com/en/problem/balanced-binary-tree/
+
+3 Binary Tree Preorder Traversal
+http://www.lintcode.com/en/problem/binary-tree-preorder-traversal/
+
+4. Binary Tree Maximum Path Sum II
+http://www.lintcode.com/en/problem/binary-tree-maximum-path-sum-ii/
+
+5. inorder Successor in Binary Search Tree
+http://www.lintcode.com/en/problem/inorder-successor-in-binary-search-tree/
+
+6. Validate Binary Search Tree
+http://www.lintcode.com/en/problem/validate-binary-search-tree/
